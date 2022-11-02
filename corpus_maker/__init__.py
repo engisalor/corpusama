4a9ea77a8f91607e8dcf19559df8d94f36857ab2
engisalor <50170623+engisalor@users.noreply.github.com>
2022-11-02 16:52:49 +0100

    fix: set up logging

---
+++ corpus_maker/__init__.py
@@ -0,0 +1,18 @@
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
+formatter = logging.Formatter(
+    "%(asctime)s - %(levelname)s - %(name)s - %(funcName)s - %(message)s"
+)
+
+log_file = ".corpus-maker.log"
+file_handler = TimedRotatingFileHandler(log_file, "midnight", backupCount=1)
+file_handler.setFormatter(formatter)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
