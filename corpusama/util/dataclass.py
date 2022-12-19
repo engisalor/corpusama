72d138dd6a532d878960d2d3a10353df0ce79bb5
engisalor <50170623+engisalor@users.noreply.github.com>
2022-12-19 18:21:52 +0100

    fix(util): remove CorpusAttribute dataclass

--- corpusama/util/dataclass.py
+++ corpusama/util/dataclass.py
@@ -1,5 +1,4 @@
 """Library dataclasses."""
-import json
 import logging
 
 from corpusama.util import util
@@ -45,38 +44,3 @@ class DocBundle:
         self.token = token
         self.xpos = xpos
         self.date = util.now()
-
-
-class CorpusAttribute:
-    """A class for corpus attributes.
-
-    Args:
-        attribute: The name of an attribute ("id", "year", "title", etc.).
-        parameters: A dictionary of parameters for creating a Sketch Engine
-            configuration file entry from the attribute.
-
-    Methods:
-        set_config_entry: Generates a string for the configuration file entry.
-
-    Example:
-        ``parameters={"DYNTYPE": "index", "MULTISEP": "|", "MULTIVALUE": "y"}``
-
-    See Also:
-        https://www.sketchengine.eu/documentation/"""
-
-    def __repr__(self):
-        return self.config_entry
-
-    def set_config_entry(self):
-        self.config_entry = f"""\nATTRIBUTE "{self.value}" """
-        if self.parameters:
-            self.config_entry += "{"
-            for k, v in self.parameters.items():
-                self.config_entry += f"\n    {k} {json.dumps(str(v))}"
-            self.config_entry += "\n    }"
-        return self.config_entry
-
-    def __init__(self, attribute: str, parameters: dict = None):
-        self.value = attribute
-        self.parameters = parameters
-        self.set_config_entry()
