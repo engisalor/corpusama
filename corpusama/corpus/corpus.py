8b9b15237861c65c08340b77375cd5be21894dfb
engisalor <50170623+engisalor@users.noreply.github.com>
2023-07-05 18:19:19 +0200

    fix: remove old methods from Corpus

--- corpusama/corpus/corpus.py
+++ corpusama/corpus/corpus.py
@@ -4,35 +4,18 @@ from corpusama.util import io as _io
 
 
 class Corpus:
-    """A class to make and maintain a corpus of vertical files.
+    """Class to make and maintain a corpus of vertical files.
 
     Args:
         config: YAML configuration file.
 
-    Methods:
-        Refer to modules in See Also.
-
     Notes:
-        - Preset for use with ReliefWeb data.
-        - Combines methods from other modules that together constitute.
-            all the steps needed for making and maintaining a corpus
-            (after raw data has been downloaded from a source).
+        Combines methods from other modules needed preparing a corpus.
+    """
 
-    See Also:
-        `archive`: Producing compressed archives of vertical files.
-        `attribute`: Generating document attributes.
-        `tagset`: Managing corpus tagsets.
-        `vertical`: Generating vertical content from raw documents."""
-
-    from corpusama.corpus.archive import export_archive, make_archive
-    from corpusama.corpus.attribute import (
-        export_attribute,
-        make_attribute,
-        unique_attribute,
-    )
+    from corpusama.corpus.attribute import make_attribute
+    from corpusama.corpus.export import export_text
     from corpusama.corpus.langid import make_langid
-    from corpusama.corpus.tagset import export_tagset
-    from corpusama.corpus.vertical import make_vertical, outdated_vert
 
     def __init__(
         self,
