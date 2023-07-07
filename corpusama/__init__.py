f180389b1a8edf070bb0dde0868dc9e95567ab98
engisalor <50170623+engisalor@users.noreply.github.com>
2023-07-07 18:02:04 +0200

    fix: auto-gen .logs/ dir

--- corpusama/__init__.py
+++ corpusama/__init__.py
@@ -1,8 +1,11 @@
 "A corpus management tool."
 import logging
+import pathlib
 from logging.handlers import TimedRotatingFileHandler
 
 log_file = ".logs/corpusama.log"
+if not pathlib.Path(".logs/").exists():
+    pathlib.Path(".logs/").mkdir()
 file_handler = TimedRotatingFileHandler(log_file, "midnight", backupCount=1)
 stream_handler = logging.StreamHandler()
 
