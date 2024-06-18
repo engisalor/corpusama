070f60a5ef7300d7abe5ae214ad8a91b844b1c5f
engisalor <50170623+engisalor@users.noreply.github.com>
2024-06-18 13:35:53 +0200

    feat: redo stanza pipeline

--- pipeline/stanza/base_pipeline.py
+++ pipeline/stanza/base_pipeline.py
@@ -11,36 +11,20 @@ On CONLL-U format, see:
     https://universaldependencies.org/format.html
 """
 
-import datetime
+import argparse
 import logging
-
-# import os
-# import tracemalloc
-import math
+import lzma
 import re
+from math import ceil
 from pathlib import Path
 from time import perf_counter
+from xml.sax.saxutils import quoteattr  # nosec
 
 import stanza
 from defusedxml.ElementTree import fromstring
+from numpy import array_split
 from stanza import DownloadMethod
 from stanza.utils.conll import CoNLL
-
-
-def convert_size(size_bytes) -> tuple:
-    """See https://stackoverflow.com/questions/5194057/"""
-    if size_bytes == 0:
-        return (0, "B")
-    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
-    i = int(math.floor(math.log(size_bytes, 1024)))
-    p = math.pow(1024, i)
-    s = round(size_bytes / p, 2)
-    return (s, size_name[i])
-
-
-def vert_to_conll(file: str) -> None:
-    """Converts a SkE-conll-formatted `.vert` file to .conll"""
-    pass
 
 
 def conll_to_vert(file: str) -> None:
@@ -53,6 +37,7 @@ def conll_to_vert(file: str) -> None:
             first_doc = True
             mwt_ids = []
             mwt_parts = []
+            n = 0
             for i, line in enumerate(f):
                 if line.startswith("# newdoc"):
                     if first_doc:
@@ -63,14 +48,20 @@ def conll_to_vert(file: str) -> None:
                     new_doc = True
                 elif line.startswith("# sent_id"):
                     if new_doc:
-                        d.write(re.sub(r"# sent_id = (\d+)", r'>\n<s id="\1">', line))
+                        n = 0
+                        d.write(f'>\n<s id="{n}">\n')
+                        # to preserve existing ids
+                        # d.write(re.sub(r"# sent_id = (\d+)", r'>\n<s id="\1">', line))
                         new_doc = False
                     else:
-                        d.write(re.sub(r"# sent_id = (\d+)", r'\n<s id="\1">', line))
+                        n += 1
+                        d.write(f'\n<s id="{n}">\n')
+                        # d.write(re.sub(r"# sent_id = (\d+)", r'\n<s id="\1">', line))
                 elif line.startswith("# text"):
                     pass
                 elif line.startswith("#"):
-                    line = re.sub(r"# (\S+) = (.*)\n", r' \1="\2"', line)
+                    tup = re.search(r"# (\S+) = (.*)\n", line).groups()
+                    line = f" {tup[0]}={quoteattr(tup[1])}"
                     d.write(line)
                 elif line.startswith(
                     ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
@@ -119,139 +110,166 @@ def conll_to_vert(file: str) -> None:
 class NLP:
     """Runs Stanza NLP on files generated from Corpusama."""
 
-    @staticmethod
-    def calculate_rate(run, _bytes, t0):
+    def _to_conll_inner(self) -> None:
+        t0 = perf_counter()
+        xml = fromstring(self.meta + "</doc>\n")
+        with open(self.dest, "a") as f:
+            f.write(
+                "# newdoc\n"
+                + "\n".join([f"# {x[0]} = {x[1]}" for x in xml.items()])
+                + "\n"
+            )
+
+        _bytes = len(self.doc.encode())
+        n_chunks = ceil(_bytes / self.batch)
+        lines = self.doc.split("\n\n")
+        arrays = array_split(lines, n_chunks)
+        for chunk in arrays:
+            docs = self.nlp.bulk_process(chunk)
+            for doc in docs:
+                CoNLL.write_doc2conll(doc, self.dest, "a")
+
         t1 = perf_counter()
         secs = t1 - t0
-        _time = str(datetime.timedelta(seconds=secs, microseconds=0))
-        # _size, _peak = tracemalloc.get_traced_memory()
-        # tracemalloc.reset_peak()
-        # tot_m, used_m, free_m = map(
-        # int, os.popen('free -t -m').readlines()[-1].split()[1:])
-        # _peak, _unit = convert_size(_peak)
-        size, unit = convert_size(_bytes)
-        msg = " - ".join(
+        id = xml.items()[0][1]
+        file_id = xml.items()[1][1]
+        msg = "".join(
             [
-                f"run {run}",
-                f"{_time}",
-                f"{size:,.2f} {unit}",
-                f"{size/secs:,.3f} {unit}/s",
-                # f"{_peak:,.2f} {_unit} RAM",
-                # f"{free_m:,.0f} MB free RAM",
+                f"{self.n} - {_bytes:,} - {len(arrays):,}/{n_chunks} chunk(s)",
+                f"- {_bytes/secs:,.0f} bytes/s - id={id} file_id={file_id}",
             ]
         )
         logging.info(msg)
+        self.doc = ""
+        self.n += 1
 
-    def to_conll_inner(self) -> None:
-        # tracemalloc.start()
-        self.run_current += 1
-        # if self.run_current % 4 == 0:
-        #     logging.info(f"run {self.run_current} - reload stanza pipeline")
-        #     self.nlp = stanza.Pipeline(
-        #         self.language,
-        #         processors=self.processors,
-        #         download_method=self.download_method,
-        #         logging_level='WARN')
-        t0 = perf_counter()
-        bytes_batch = sum([len(x[1].encode()) for x in self.docs])
-        self.xml_headers = [x[0] for x in self.docs]
-        self.docs = self.nlp.bulk_process([x[1] for x in self.docs])
-        for x in range(len(self.docs)):
-            self.docs[x].xml_header = self.xml_headers[x]
-            self.docs[x].xml_footer = "</doc>\n"
-        for doc in self.docs:
-            xml = fromstring(doc.xml_header + doc.xml_footer)
-            with open(self.dest, "a") as f:
-                f.write(
-                    "# newdoc\n"
-                    + "\n".join([f"# {x[0]} = {x[1]}" for x in xml.items()])
-                    + "\n"
-                )
-            CoNLL.write_doc2conll(doc, self.dest, "a")
-        self.docs = []
-        self.bytes_processed += bytes_batch
-        self.calculate_rate(self.run_current, bytes_batch, t0)
+    def _process_file(self, f):
+        for i, line in enumerate(f):
+            if line.startswith('<doc id="'):
+                self.meta = line
+                self.doc = ""
+            elif line.startswith("</doc>"):
+                self._to_conll_inner()
+            else:
+                self.doc += line
+        if self.doc:
+            self._to_conll_inner()
 
     def to_conll(self) -> None:
-        t0 = perf_counter()
         self.dest.unlink(missing_ok=True)
-        with open(self.source) as f:
-            for i, line in enumerate(f):
-                if line.startswith('<doc id="'):
-                    self.meta = line
-                    self.doc = ""
-                elif line.startswith("</doc>"):
-                    self.docs.append((self.meta, self.doc))
-                    _bytes = sum([len(doc[1].encode()) for doc in self.docs])
-                    if _bytes >= self.chunksize:
-                        self.to_conll_inner()
-                else:
-                    self.doc += line
-            if self.docs:
-                self.to_conll_inner()
-        self.calculate_rate("TOTAL", self.bytes_processed, t0)
+        self.nlp = stanza.Pipeline(
+            self.language,
+            processors=self.processors,
+            download_method=self.download_method,
+            logging_level="WARN",
+        )
+        if self.source.suffix == ".xz":
+            with lzma.open(self.source, "rt") as f:
+                self._process_file(f)
+        else:
+            with open(self.source) as f:
+                self._process_file(f)
 
     def verify(self) -> None:
-        def inner(file: Path) -> int:
-            if not file.exists() or file.suffix not in [".txt", ".conllu"]:
-                raise ValueError(f"{file.suffix} not implemented or no documents found")
+        def process_file(file: Path, f) -> int:
             docs = 0
-            with open(file) as f:
-                for line in f:
-                    if file.suffix in [".txt"] and line.startswith("<doc id="):
-                        docs += 1
-                    elif file.suffix in file.suffix in [".conllu"] and line.startswith(
-                        "# newdoc"
-                    ):
-                        docs += 1
-            msg = f"verify: {docs:,} docs in {file}"
-            logging.info(msg)
+            for line in f:
+                if ".conllu" in file.suffixes and line.startswith("# newdoc"):
+                    docs += 1
+                elif line.startswith("<doc id="):
+                    docs += 1
             return docs
 
-        source_docs = inner(self.source)
-        dest_docs = inner(self.dest)
+        def inner(file: Path) -> int:
+            if self.source.suffix == ".xz":
+                with lzma.open(self.source, "rt") as f:
+                    docs = process_file(file, f)
+            else:
+                with open(file) as f:
+                    docs = process_file(file, f)
+            logging.info(f"verify - {docs:,} docs in {file}")
+            return docs
 
-        msg = f"source & dest have unequal doc counts {source_docs} != {dest_docs}"
-        if not source_docs == dest_docs:
-            logging.warning(f"verify: {msg}")
-        else:
-            logging.info("verify: source & dest have same number of docs")
+        s = inner(self.source)
+        d = inner(self.dest)
+        v = None
+        if self.source.with_suffix(".vert").exists():
+            v = inner(self.source.with_suffix(".vert"))
+        if not s == d:
+            raise ValueError(f"verify - doc counts {s} != {d} .conllu")
+        if v and v != s:
+            raise ValueError(f"verify - doc counts {s} != {v} .vert")
 
     def __init__(
         self,
         file: str,
         language: str,
-        processors: str,
-        chunksize: float = 1,
-        download_method: DownloadMethod | None = None,
+        processors: str = "tokenize,mwt,pos,lemma,depparse",
+        batch: float = 1024,
+        download_method: DownloadMethod = DownloadMethod.REUSE_RESOURCES,
     ) -> None:
         self.file = file
         self.source = Path(self.file)
         self.dest = self.source.with_suffix(".conllu")
-        self.docs = []
         self.doc = ""
         self.meta = ""
-        self.chunksize = (1024**2) * chunksize
-        self.run_current = 0
-        self.bytes_processed = 0
+        self.n = 0
+        self.batch = batch
         self.language = language
         self.processors = processors
         self.download_method = download_method
-        self.nlp = stanza.Pipeline(
-            language,
-            processors=processors,
-            download_method=download_method,
-            logging_level="WARN",
-        )
-        # tot_m, used_m, free_m = map(
-        #     int, os.popen('free -t -m').readlines()[-1].split()[1:])
-        msg = " ".join(
-            [
-                f"{self.file} ({language}) -> Stanza (~{chunksize} MB chunks)",
-                # f" -> CoNLL ({free_m:,.0f} MB free RAM)",
-            ]
-        )
 
-        logging.info(msg)
-        self.to_conll()
-        self.verify()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process some integers.")
+    parser.add_argument(
+        "-b",
+        "--batch",
+        type=int,
+        help="Bytes to process per file chunk, default `1024`",
+        default=1024,
+    )
+    parser.add_argument(
+        "-n",
+        "--nlp",
+        help="Stanza processors, e.g. `tokenize,mwt,pos,lemma,depparse`",
+        default="tokenize,mwt,pos,lemma,depparse",
+    )
+    parser.add_argument(
+        "-c",
+        "--conll",
+        action="store_true",
+        help="Run the pipeline and output .conllu files",
+    )
+    parser.add_argument(
+        "-s", "--ske", action="store_true", help="Language code for Stanza, e.g. `en`"
+    )
+    parser.add_argument(
+        "-v",
+        "--verify",
+        action="store_true",
+        help="Compare source files with output files",
+    )
+    parser.add_argument(
+        "-V", "--verbose", action="store_true", help="Verbose logging (`logging.INFO`)"
+    )
+    parser.add_argument("lang", help="Language code for Stanza, e.g. `en`")
+    parser.add_argument("file", nargs="+", help="Text file(s) to process")
+    args = parser.parse_args()
+
+    if args.verbose:
+        level = logging.INFO
+    else:
+        level = logging.WARNING
+    logging.basicConfig(format="%(asctime)s %(message)s", level=level)
+
+    for file in args.file:
+        file = Path(file)
+        nlp = NLP(file, args.lang, args.nlp, args.batch)
+
+        if args.conll:
+            nlp.to_conll()
+        if args.ske:
+            conll_to_vert(file.with_suffix(".conllu"))
+        if args.verify:
+            nlp.verify()
