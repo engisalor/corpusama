aa525287e78fb1e051d5ecfa29b0867bb2edaa0f
engisalor <50170623+engisalor@users.noreply.github.com>
2024-06-20 21:02:57 +0200

    feat: working stanza pipeline

--- pipeline/stanza/base_pipeline.py
+++ pipeline/stanza/base_pipeline.py
@@ -9,22 +9,89 @@ On SkE's SkE-conll format, see:
 
 On CONLL-U format, see:
     https://universaldependencies.org/format.html
+
+Notes:
+    `uninorm` module from Unitok: Michelfeit et al., 2014; Rychlý & Špalek, 2022.
+    License and code available at <https://corpus.tools/wiki/Unitok>.
 """
 
 import argparse
 import logging
 import lzma
+import os
 import re
+
+# import subprocess
 from math import ceil
 from pathlib import Path
 from time import perf_counter
 from xml.sax.saxutils import quoteattr  # nosec
 
 import stanza
+import uninorm_4 as uninorm
 from defusedxml.ElementTree import fromstring
+from nltk import download as nltk_download
+from nltk import sent_tokenize
 from numpy import array_split
 from stanza import DownloadMethod
 from stanza.utils.conll import CoNLL
+
+nltk_langs = {
+    "en": "english",
+    "fr": "french",
+    "es": "spanish",
+}
+
+
+# def spellcheck(word, dictionaries="en_US,es_ES,fr_FR") -> bool:
+#     """Checks if a word exists in hunspell dictionaries."""
+#     if len(word.split()) > 1:
+#         raise ValueError(f"word cannot include whitespace: '{word}'")
+#     result = subprocess.run(
+#         ["hunspell", "-d", dictionaries], input=word.encode(), capture_output=True
+#     )
+#     hunspell = result.stdout.decode()
+#     hunspell = [x for x in hunspell.split("\n")[1:] if x][0][0]
+#     if hunspell in ["&", "#"]:
+#         return False
+#     else:
+#         return True
+
+
+def wrap_lines(match, hunspell: bool = False, dictionaries="en_US,es_ES,fr_FR"):
+    """Joins words split by a dash and new line if found in hunspell dictionaries."""
+    candidate = "".join(match.groups())
+
+    if not hunspell:
+        # logging.debug(f"wrap -{candidate}")
+        return candidate + "\n"
+    # elif spellcheck(candidate, dictionaries):
+    #     # logging.debug(f"wrap -{candidate}")
+    #     return candidate + "\n"
+    else:
+        # logging.debug(f"reject -{candidate}")
+        return match.group()
+
+
+def clean_text(text: str) -> str:
+    """Cleans texts to prepare for passing to an NLP pipeline."""
+    lines = text.split("\n")
+    lines = [uninorm.normalize_line(x) for x in lines]
+    return "".join(lines)
+
+
+def splice_mwt_lines(mwt_parts):
+    """Splice multiword term CoNLLU lines into a Sketch Engine compatible format."""
+    old = list(
+        map(
+            ",".join,
+            zip(*[mwt_parts[x] for x in range(len(mwt_parts)) if x > 0]),
+        )
+    )
+    new = [(x if not len(set(x.split(","))) == 1 else x.split(",")[0]) for x in old]
+    new[1] = mwt_parts[0][1]
+    new[-1] = mwt_parts[0][-1]
+    return "\t".join(new) + "\n"
 
 
 def conll_to_vert(file: str) -> None:
@@ -39,6 +106,7 @@ def conll_to_vert(file: str) -> None:
             mwt_parts = []
             n = 0
             for i, line in enumerate(f):
+                # write document tag
                 if line.startswith("# newdoc"):
                     if first_doc:
                         d.write("<doc")
@@ -46,6 +114,7 @@ def conll_to_vert(file: str) -> None:
                     else:
                         d.write("\n</doc>\n<doc")
                     new_doc = True
+                # write sentence tag
                 elif line.startswith("# sent_id"):
                     if new_doc:
                         n = 0
@@ -57,54 +126,60 @@ def conll_to_vert(file: str) -> None:
                         n += 1
                         d.write(f'\n<s id="{n}">\n')
                         # d.write(re.sub(r"# sent_id = (\d+)", r'\n<s id="\1">', line))
+                # ignore text line
                 elif line.startswith("# text"):
                     pass
+                # write metadata
                 elif line.startswith("#"):
                     tup = re.search(r"# (\S+) = (.*)\n", line).groups()
                     line = f" {tup[0]}={quoteattr(tup[1])}"
                     d.write(line)
+                # parse content
                 elif line.startswith(
                     ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
                 ):
+                    # activate mwt line logic
                     if re.match(r"\d+-\d+", line):
+                        # write any preceding mwt line group
+                        if mwt_ids:
+                            spliced = splice_mwt_lines(mwt_parts)
+                            d.write(spliced)
+                            # d.write(line)
+                            mwt_parts = []
+                            mwt_ids = []
+                        # activate mwt line collection
                         ids = [int(x) for x in line.split("\t")[0].split("-")]
                         mwt_ids = [str(x) for x in range(ids[0], ids[1] + 1)]
-                        mwt_ids_len = int(mwt_ids[-1])
+                    # parse mwt content
                     if mwt_ids:
                         id = re.match(r"\d+", line).group()
-                        if id in mwt_ids and len(mwt_parts) < mwt_ids_len:
+                        # collect mwt line member
+                        if id in mwt_ids:
                             mwt_parts.append([x.strip() for x in line.split("\t")])
+                        # write mwt line group
                         else:
-                            new = list(
-                                map(
-                                    ",".join,
-                                    zip(
-                                        *[
-                                            mwt_parts[x]
-                                            for x in range(len(mwt_parts))
-                                            if x > 0
-                                        ]
-                                    ),
-                                )
-                            )
-                            new = [
-                                (
-                                    x
-                                    if not len(set(x.split(","))) == 1
-                                    else x.split(",")[0]
-                                )
-                                for x in new
-                            ]
-                            new[1] = mwt_parts[0][1]
-                            new[-1] = mwt_parts[0][-1]
-                            d.write("\t".join(new) + "\n")
+                            spliced = splice_mwt_lines(mwt_parts)
+                            d.write(spliced)
                             d.write(line)
                             mwt_parts = []
                             mwt_ids = []
+                    # write single-word line
                     else:
                         d.write(line)
+                # activate end of sentence logic
                 else:
+                    # sanity check
+                    if line.strip():
+                        raise ValueError(f"unused non-empty line: {line}")
+                    # write any trailing mwt group
+                    if mwt_ids:
+                        spliced = splice_mwt_lines(mwt_parts)
+                        d.write(spliced)
+                        mwt_parts = []
+                        mwt_ids = []
+                    # end sentence tag
                     d.write("</s>")
+            # end document tag
             d.write("\n</doc>")
 
 
@@ -114,6 +189,19 @@ class NLP:
     def _to_conll_inner(self) -> None:
         t0 = perf_counter()
         xml = fromstring(self.meta + "</doc>\n")
+        id = xml.items()[0][1]
+        file_id = xml.items()[1][1]
+        if self.uninorm:
+            doc = clean_text(self.doc)
+            logging.debug(f"{self.n} - cleaned - {id}/{file_id}")
+        else:
+            doc = self.doc
+        if self.wrap:
+            a = re.compile(r"( \w+)-\n(\w+ *)")
+            doc = a.sub(wrap_lines, doc)
+            logging.debug(f"{self.n} - wrapped - {id}/{file_id}")
+        _bytes = len(doc.encode())
+
         with open(self.dest, "a") as f:
             f.write(
                 "# newdoc\n"
@@ -121,25 +209,47 @@ class NLP:
                 + "\n"
             )
 
-        _bytes = len(self.doc.encode())
-        n_chunks = ceil(_bytes / self.batch)
-        lines = self.doc.split("\n\n")
-        arrays = array_split(lines, n_chunks)
-        for chunk in arrays:
-            docs = self.nlp.bulk_process(chunk)
-            for doc in docs:
-                CoNLL.write_doc2conll(doc, self.dest, "a")
+        if _bytes > self.big:
+            doc = sent_tokenize(doc, self.nltk_lang)
+            logging.debug(f"{self.n} - split - {id}/{file_id}")
+            n_chunks = ceil(_bytes / self.chunk)
+            array = array_split(doc, n_chunks)
+            array = [x for x in array if x.size > 0]
+            for chunk in array:
+                _size = len("\n".join(chunk).encode())
+                if not _size > self.chunk * self.force:
+                    logging.debug(f"{self.n} - chunk - {_size}")
+                    docs = self.nlp.bulk_process(chunk)
+                    for d in docs:
+                        CoNLL.write_doc2conll(d, self.dest, "a")
+                else:
+                    logging.debug(f"{self.n} - big chunk - {_size}")
+                    for unit in chunk:
+                        __size = len(unit.encode())
+                        if __size > self.chunk:
+                            lines = unit.split("\n")
+                            n_mini = ceil(__size / self.chunk)
+                            mini_array = array_split(lines, n_mini)
+                            mini_chunk = ["\n".join(x) for x in mini_array]
+                            mini_docs = self.nlp.bulk_process(mini_chunk)
+                            for doc in mini_docs:
+                                CoNLL.write_doc2conll(d, self.dest, "a")
+                            m = (
+                                f"{self.n} - big chunk - force split - first 100 chars"
+                                + " - {repr(mini_chunk[0][:100])}"
+                            )
+                            logging.debug(m)
+                        else:
+                            doc = self.nlp.process(unit)
+                            CoNLL.write_doc2conll(doc, self.dest, "a")
+        else:
+            logging.debug(f"{self.n} - neural - {id}/{file_id}")
+            doc = self.nlp.process(doc)
+            CoNLL.write_doc2conll(doc, self.dest, "a")
 
         t1 = perf_counter()
         secs = t1 - t0
-        id = xml.items()[0][1]
-        file_id = xml.items()[1][1]
-        msg = "".join(
-            [
-                f"{self.n} - {_bytes:,} - {len(arrays):,}/{n_chunks} chunk(s)",
-                f"- {_bytes/secs:,.0f} bytes/s - id={id} file_id={file_id}",
-            ]
-        )
+        msg = f"{self.n} - {_bytes:,} - {_bytes/secs:,.0f} b/s - {id}/{file_id}"
         logging.info(msg)
         self.doc = ""
         self.n += 1
@@ -156,14 +266,23 @@ class NLP:
         if self.doc:
             self._to_conll_inner()
 
-    def to_conll(self) -> None:
-        self.dest.unlink(missing_ok=True)
+    def start_nlp(self):
+        nltk_download("punkt", quiet=True)
         self.nlp = stanza.Pipeline(
             self.language,
             processors=self.processors,
             download_method=self.download_method,
             logging_level="WARN",
+            tokenize_batch_size=32,  # 32
+            mwt_batch_size=50,  # 50
+            pos_batch_size=100,  # 100
+            lemma_batch_size=50,  # 50
+            depparse_batch_size=200,  # 5000
         )
+
+    def to_conll(self) -> None:
+        self.dest.unlink(missing_ok=True)
+        self.start_nlp()
         if self.source.suffix == ".xz":
             with lzma.open(self.source, "rt") as f:
                 self._process_file(f)
@@ -206,7 +325,11 @@ class NLP:
         file: str,
         language: str,
         processors: str = "tokenize,mwt,pos,lemma,depparse",
-        batch: float = 1024,
+        uninorm: bool = True,
+        wrap: bool = True,
+        big: int = 0,
+        chunk: int = 1000,
+        force: int = 10,
         download_method: DownloadMethod = DownloadMethod.REUSE_RESOURCES,
     ) -> None:
         self.file = file
@@ -215,26 +338,56 @@ class NLP:
         self.doc = ""
         self.meta = ""
         self.n = 0
-        self.batch = batch
+        self.uninorm = uninorm
+        self.wrap = wrap
+        self.big = big
+        self.chunk = chunk
+        self.force = force
         self.language = language
+        self.nltk_lang = nltk_langs[language]
         self.processors = processors
         self.download_method = download_method
+        logging.info(f"file={file}")
+        logging.info(f"uninorm={uninorm}")
+        logging.info(f"wrap={wrap}")
+        logging.info(f"big={big:,}")
+        logging.info(f"chunk={chunk:,}")
+        logging.info(f"force={force:,}")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process some integers.")
     parser.add_argument(
-        "-b",
-        "--batch",
-        type=int,
-        help="Bytes to process per file chunk, default `1024`",
-        default=1024,
+        "--environ",
+        nargs="*",
+        default=["PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True"],
+        help="Environment variables",
     )
     parser.add_argument(
-        "-n",
         "--nlp",
-        help="Stanza processors, e.g. `tokenize,mwt,pos,lemma,depparse`",
         default="tokenize,mwt,pos,lemma,depparse",
+        help="Stanza processors, e.g. `tokenize,mwt,pos,lemma,depparse`",
+    )
+    parser.add_argument(
+        "--big",
+        type=int,
+        default=0,
+        help="""Disable neural sentence segmentation at n bytes of text
+          and use rule-based segmentation instead (default `0` never use neural)""",
+    )
+    parser.add_argument(
+        "--chunk",
+        type=int,
+        default=1000,
+        help="""Desired chunksize in bytes to feed Stanza for big docs
+          (default `1000`; permits longer sentences until `force` size is reached""",
+    )
+    parser.add_argument(
+        "--force",
+        type=int,
+        default=10,
+        help="""Largest multiple of chunksize allowed before forcing
+        smaller sentence segmentation (default `10`, e.g., `10*chunk` bytes)""",
     )
     parser.add_argument(
         "-c",
@@ -246,27 +399,49 @@ if __name__ == "__main__":
         "-s", "--ske", action="store_true", help="Language code for Stanza, e.g. `en`"
     )
     parser.add_argument(
+        "-u", "--uninorm", action="store_true", help="Clean docs with uninorm"
+    )
+    parser.add_argument(
+        "-w", "--wrap", action="store_true", help="Remove hyphens in line-wrapped words"
+    )
+    parser.add_argument(
         "-v",
         "--verify",
         action="store_true",
         help="Compare source files with output files",
     )
-    parser.add_argument(
-        "-V", "--verbose", action="store_true", help="Verbose logging (`logging.INFO`)"
-    )
+    parser.add_argument("-V", "--verbose", action="store_true", help="Verbose logging")
+    parser.add_argument("-D", "--debug", action="store_true", help="Debug logging")
+
     parser.add_argument("lang", help="Language code for Stanza, e.g. `en`")
     parser.add_argument("file", nargs="+", help="Text file(s) to process")
     args = parser.parse_args()
 
-    if args.verbose:
+    if args.debug:
+        level = logging.DEBUG
+    elif args.verbose:
         level = logging.INFO
     else:
         level = logging.WARNING
     logging.basicConfig(format="%(asctime)s %(message)s", level=level)
 
+    for arg in args.environ:
+        environ = arg.split("=")
+        os.environ[environ[0]] = environ[1]
+        logging.info(f"{environ[0]}={os.environ[environ[0]]}")
+
     for file in args.file:
         file = Path(file)
-        nlp = NLP(file, args.lang, args.nlp, args.batch)
+        nlp = NLP(
+            file,
+            args.lang,
+            args.nlp,
+            args.uninorm,
+            args.wrap,
+            args.big,
+            args.chunk,
+            args.force,
+        )
 
         if args.conll:
             nlp.to_conll()
---
+++ pipeline/stanza/uninorm_4.py
@@ -0,0 +1,213 @@
+#!/usr/bin/python3
+
+__version__ = "4.0"
+
+import html
+import io
+import re
+from unicodedata import category, normalize
+
+TAG_RE = re.compile(
+    r"""<!--.*?-->|<[!?/]?[^\W\d][\w:\.-]*(\s+[^\W\d][\w:\.-]*\s*=\s*('[^']*'|"[^"]*"))*\s*/?\s*>""",  # noqa: E501
+    re.UNICODE,
+)
+CHARREF = re.compile(
+    r"&(#[0-9]+;|#[xX][0-9a-fA-F]+;|[^\t\n\f <&#;]{1,32};)"
+)  # html._charref, but with compulsory semicolon
+BASIC_XML_ENTITIES = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&apos;",
+}
+
+
+def replace_charref(s):
+    replacement = html._replace_charref(s)
+    if replacement in BASIC_XML_ENTITIES:
+        return BASIC_XML_ENTITIES[replacement]
+    return replacement
+
+
+def replace_html_entities(text):
+    if "&" not in text:
+        return text
+    return CHARREF.sub(replace_charref, text)
+
+
+def remove_control_chars(text, exceptions=[]):
+    return "".join(
+        [c for c in text if c in exceptions or not category(c).startswith("C")]
+    )
+
+
+def normalize_spaces(text):
+    return "".join([(" " if category(c) == "Zs" else c) for c in text])
+
+
+SINGLE_QUOTE_RE = re.compile(
+    "[\u0027\u0060\u00b4\u02bc\u055a\u07f4\u07f5\uff07\u2018\u2019\u201a\u201b\u2039\u203a\u275b\u275c\u02b9\u2032\u2035]"  # noqa: E501
+)
+DOUBLE_QUOTE_RE = re.compile(
+    "[\u0022\u276e\u276f\uff02\u201c\u201d\u201e\u201f\u301d\u301e\u301f\u275d\u275e\u2033\u2036\u02ba\u02ee]"  # noqa: E501
+)
+
+
+def normalize_quotes(text):
+    text = SINGLE_QUOTE_RE.sub("'", text)
+    text = DOUBLE_QUOTE_RE.sub('"', text)
+    return text
+
+
+HYPHEN_RE = re.compile(
+    "[\u002d\u058a\u05be\u1400\u1806\u2010\u2011\u2e17\u2e1a\u30a0\ufe63\uff0d]"
+)
+DASH_RE = re.compile(
+    "[\u2012\u2013\u2014\u2015\u2e3a\u2e3b\u2e40\u301c\u3030\ufe31\ufe32\ufe58]"
+)
+
+
+def normalize_dashes(text):
+    text = HYPHEN_RE.sub("-", text)
+    text = DASH_RE.sub("\u2013", text)
+    return text
+
+
+NEWLINE = {"unix": "\n", "dos": "\r\n"}
+
+
+def normalize_line(
+    line,
+    normal_form="NFKC",
+    tab="space",
+    keep_chars=["200D"],
+    new_line="unix",
+    keep_quotes=False,
+    keep_dashes=False,
+    dont_strip=False,
+    keep_empty=False,
+):
+    line = replace_html_entities(line)
+    line = normalize_spaces(line)
+    # don't break tags
+    parts = []
+    pos = 0
+    for mo in TAG_RE.finditer(line):
+        startpos, endpos = mo.span()
+        if startpos > pos:
+            betweentags = line[pos:startpos]
+            betweentags = normalize(normal_form, betweentags)
+            if not keep_quotes:
+                betweentags = normalize_quotes(betweentags)
+            if not keep_dashes:
+                betweentags = normalize_dashes(betweentags)
+            parts.append(betweentags)
+        tag = line[startpos:endpos]
+        tag = normalize("".join([c for c in normal_form if c != "K"]), tag)
+        parts.append(tag)
+        pos = endpos
+    if pos < len(line):
+        betweentags = line[pos:]
+        betweentags = normalize(normal_form, betweentags)
+        if not keep_quotes:
+            betweentags = normalize_quotes(betweentags)
+        if not keep_dashes:
+            betweentags = normalize_dashes(betweentags)
+        parts.append(betweentags)
+    line = "".join(parts)
+    # control characters
+    if tab == "space":
+        line = line.replace("\t", " ")
+    elif tab == "none":
+        line = line.replace("\t", "")
+    line = remove_control_chars(line, exceptions=[chr(int(c, 16)) for c in keep_chars])
+    lines = line.replace("\u2028", "\n").replace("\u2029", "\n").split("\n")
+    # print out
+    result = ""
+    for line in lines:
+        if not dont_strip:
+            line = line.strip()
+        if keep_empty or len(line) > 0:
+            result += line
+            result += NEWLINE[new_line]
+    return result
+
+
+if __name__ == "__main__":
+    import argparse
+    import sys
+
+    parser = argparse.ArgumentParser(
+        description="Universal text normalizer for linguistic research. Output is always UTF-8."  # noqa: E501
+    )
+    parser.add_argument(
+        "-e", "--encoding", help="input encoding (default:'utf_8')", default="utf_8"
+    )
+    parser.add_argument(
+        "-d",
+        "--decoding-errors",
+        help="treatment of decoding errors (default:'replace', 'ignore' or 'strict')",
+        default="replace",
+    )
+    parser.add_argument(
+        "-f",
+        "--normal-form",
+        help="Unicode normal form (default:'NFKC', 'NFC', 'NFKD' or 'NFD')",
+        default="NFKC",
+    )
+    parser.add_argument(
+        "-t",
+        "--tab",
+        help="tab replacement (default:'space', 'tab' or 'none')",
+        default="space",
+    )
+    parser.add_argument(
+        "-c",
+        "--keep-chars",
+        help="control characters to keep, space-separated hexadecimal numbers (default:200D)",  # noqa: E501
+        nargs="*",
+        default=["200D"],
+    )
+    parser.add_argument(
+        "-n",
+        "--new-line",
+        help="new line character (default:'unix' or 'dos')",
+        default="unix",
+    )
+    parser.add_argument(
+        "-q",
+        "--keep-quotes",
+        help="keep various quotes and apostrophes (default:off)",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-a",
+        "--keep-dashes",
+        help="keep various dashes and hyphens (default:off)",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-s",
+        "--dont-strip",
+        help="keep leading and trailing whitespace (default:off)",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-z", "--keep-empty", help="keep empty lines (default:off)", action="store_true"
+    )
+    parser.add_argument(
+        "-v", "--version", action="version", version="%(prog)s " + __version__
+    )
+    args = parser.parse_args()
+    kwargs = vars(args).copy()
+    del kwargs["encoding"]
+    del kwargs["decoding_errors"]
+
+    stdin = io.TextIOWrapper(
+        sys.stdin.buffer, encoding=args.encoding, errors=args.decoding_errors
+    )
+    stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8")
+    for line in stdin:
+        normalized = normalize_line(line, **kwargs)
+        stdout.write(normalized)
