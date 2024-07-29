6a197938fc8dc2cdba4bbdbd64a63fc841a27d57
engisalor <50170623+engisalor@users.noreply.github.com>
2024-07-29 18:17:59 +0200

    test: add mwe stanza pipeline tests

--- test/test_pipeline/test_stanza.py
+++ test/test_pipeline/test_stanza.py
@@ -1,59 +1,140 @@
-# import unittest
-# from dataclasses import dataclass
+import lzma
+import re
+import unittest
+from hashlib import blake2b
+from pathlib import Path
 
-# import stanza
+from click.testing import CliRunner
 
-# from corpusama.corpus import stanza as _stanza
-# from corpusama.util.dataclass import DocBundle
-
-
-# @dataclass
-# class Word:
-#     text: str = "is"
-#     lemma: str = "be"
-#     xpos: str = "VBZ"
+from pipeline.stanza import base_pipeline
 
 
-# resources = ".local-only/stanza_resources"
-# processors = "tokenize,pos,lemma"
-# text = """This is an example sentence with the number 5 and symbol &.
-# Another sentence could include a URL, like www.google.com."""
+def file_hash(file):
+    with open(file, "rb") as f:
+        txt = f.read()
+    return blake2b(txt).digest().hex()
 
 
-# class Test_Stanza_Load(unittest.TestCase):
-#     def test_load_nlp(self):
-#         nlp = _stanza.load_nlp("en", processors)
-#         self.assertEqual(type(nlp), stanza.Pipeline)
+class TestOtherFuncs(unittest.TestCase):
+    def test_wrap_lines(self):
+        doc = "A wra-\npped word."
+        ref = "A wrapped \nword."
+        a = re.compile(r"( \w+)-\n(\w+ *)")
+        doc = a.sub(base_pipeline.wrap_lines, doc)
+        self.assertEqual(ref, doc)
 
 
-# class Test_Stanza_Usage(unittest.TestCase):
-#     @classmethod
-#     def setUpClass(cls):
-#         cls.nlp = _stanza.load_nlp("en", processors)
-#         cls.doc = cls.nlp.process(text)
+class TestConllToVert(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.runner = CliRunner()
+        cls.rw_en_file = Path("test/test_pipeline/files/rw_en.txt.conllu")
+        cls.splice_mwt_file = Path("test/test_pipeline/files/splice_mwt.conllu")
+        cls.rw_en_file_xz = cls.rw_en_file.with_suffix(".conllu.xz")
+        with open(cls.rw_en_file) as source:
+            with lzma.open(cls.rw_en_file_xz, "w") as o:
+                o.write(source.read().encode())
 
-#     def test_get_xpos(self):
-#         xpos = _stanza.get_xpos([self.doc])
-#         test_xpos = [",", ".", "NN", "VB"]
-#         for x in test_xpos:
-#             self.assertIn(x, xpos)
+    def tearDownClass(cls):
+        cls.rw_en_file_xz.unlink(missing_ok=True)
 
-#     def test_fix_lemma_number(self):
-#         word_5 = self.doc.sentences[0].words[8]
-#         self.assertEqual(_stanza.fix_lemma(word_5), "[number]")
+    def test_conll_to_vert(self):
+        """Compares file hashes. Use diff on out & ref if assertEqual fails."""
+        f = str(self.rw_en_file)
+        out = self.rw_en_file.with_suffix(".vert")
+        ref = self.rw_en_file.with_suffix(".vert.REF")
+        result = self.runner.invoke(base_pipeline.conll_to_vert, [f, "--no-compress"])
+        self.assertEqual(file_hash(out), file_hash(ref))
+        self.assertTrue(result.exit_code == 0)
+        out.unlink()
 
-#     def test_fix_lemma_do_nothing(self):
-#         word_This = self.doc.sentences[0].words[0]
-#         self.assertEqual(_stanza.fix_lemma(word_This), "this")
+    def test_conll_to_vert_splice_mwt(self):
+        """Compares file hashes. Use diff on out & ref if assertEqual fails.
 
-#     def test_fix_lemma_no_lemma(self):
-#         word = Word(lemma=None)
-#         self.assertEqual(_stanza.fix_lemma(word), "is")
+        <doc id="EN test cases">:
+        - <s id="0"> `can't` (middle, hypen)
+        - <s id="1"> `cannot` (no hypen)
+        - <s id="2"> `families'` (start, end, plural possessive)
+        - <s id="3"> `Stakeholders'` (start, preceded by mwt w/ same positions 1-2)
+        - <s id="4"> `Girl's` (singular possessive)
+        - <s id="5"> `wasn't`
+        - <s id="6"> `Don't`
 
-#     def test_run(self):
-#         bundle = _stanza.run([text], [1], self.nlp)
-#         self.assertIsInstance(bundle, DocBundle)
+        <doc id="ES test cases">:
+        - <s id="0"> `del` (de+el)
+        - <s id="1"> `decirlo` (verb+el)
+        - <s id="2"> `apropiárselo` (three tokens)
+        """
+        f = str(self.splice_mwt_file)
+        out = self.splice_mwt_file.with_suffix(".vert")
+        ref = self.splice_mwt_file.with_suffix(".vert.REF")
+        result = self.runner.invoke(base_pipeline.conll_to_vert, [f, "--no-compress"])
+        self.assertEqual(file_hash(out), file_hash(ref))
+        self.assertTrue(result.exit_code == 0)
+        out.unlink()
+
+    def test_conll_to_vert_read_xz(self):
+        """Compares file hashes. Use diff on out & ref if assertEqual fails."""
+        f = str(self.rw_en_file)
+        out = self.rw_en_file.with_suffix(".vert")
+        ref = self.rw_en_file.with_suffix(".vert.REF")
+        result = self.runner.invoke(base_pipeline.conll_to_vert, [f, "--no-compress"])
+        self.assertTrue(result.exit_code == 0)
+        self.assertEqual(file_hash(out), file_hash(ref))
+        out.unlink(missing_ok=True)
+
+    def test_conll_to_vert_write_xz(self):
+        """Checks if output file exists."""
+        f = str(self.rw_en_file)
+        out = self.rw_en_file.with_suffix(".vert.xz")
+        result = self.runner.invoke(base_pipeline.conll_to_vert, [f, "--compress"])
+        self.assertTrue(result.exit_code == 0)
+        self.assertTrue(out.exists())
+        out.unlink()
+
+    def test_conll_to_vert_write_xz_keep(self):
+        """Checks if output file exists."""
+        f = str(self.rw_en_file)
+        out = self.rw_en_file.with_suffix(".vert")
+        out_xz = self.rw_en_file.with_suffix(".vert.xz")
+        result = self.runner.invoke(
+            base_pipeline.conll_to_vert, [f, "--compress", "--keep"]
+        )
+        self.assertTrue(result.exit_code == 0)
+        self.assertTrue(out.exists())
+        self.assertTrue(out_xz.exists())
+        out.unlink()
+        out_xz.unlink()
+
+    def test_conll_to_vert_write_xz_no_force(self):
+        """Checks if output file exists."""
+        f = str(self.rw_en_file)
+        out = self.rw_en_file.with_suffix(".vert")
+        out_xz = self.rw_en_file.with_suffix(".vert.xz")
+        # no compress
+        result = self.runner.invoke(
+            base_pipeline.conll_to_vert, [f, "--no-compress", "--force"]
+        )
+        self.assertTrue(result.exit_code == 0)
+        result2 = self.runner.invoke(
+            base_pipeline.conll_to_vert, [f, "--no-compress", "--no-force"]
+        )
+        self.assertTrue(result2.exit_code == 1)
+        self.assertTrue("already exists" in str(result2.exception))
+        # compress, only .xz exists
+        result3 = self.runner.invoke(
+            base_pipeline.conll_to_vert, [f, "--compress", "--force"]
+        )
+        self.assertTrue(result3.exit_code == 0)
+        out.unlink(missing_ok=True)
+        result4 = self.runner.invoke(
+            base_pipeline.conll_to_vert, [f, "--compress", "--no-force"]
+        )
+        self.assertTrue(result4.exit_code == 1)
+        self.assertTrue("already exists" in str(result4.exception))
+        out.unlink(missing_ok=True)
+        out_xz.unlink(missing_ok=True)
 
 
-# if __name__ == "__main__":
-#     unittest.main()
+if __name__ == "__main__":
+    unittest.main()
