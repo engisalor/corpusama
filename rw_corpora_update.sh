ba35b182e6aa4a09f24d5f19a1517d3ad73bb253
engisalor <50170623+engisalor@users.noreply.github.com>
2024-10-11 16:19:57 +0200

    fix: rw scripts

--- rw_corpora_update.sh
+++ rw_corpora_update.sh
@@ -10,13 +10,13 @@ fi
 echo "... get and process source data"
 python3 rw_corpora_update.py "${1}" "${2}" || exit 1
 echo "... convert source files to .conllu (run Stanza NLP)"
-ES_FILES=$(find -name reliefweb_es*.txt)
-FR_FILES=$(find -name reliefweb_fr*.txt)
-EN_FILES=$(find -name reliefweb_en*.txt)
+ES_FILES=$(find -name "reliefweb_es*.txt")
+FR_FILES=$(find -name "reliefweb_fr*.txt")
+EN_FILES=$(find -name "reliefweb_en*.txt")
 python3 ./pipeline/stanza/base_pipeline.py to-conll -l es $ES_FILES || exit 1
 python3 ./pipeline/stanza/base_pipeline.py to-conll -l fr $FR_FILES || exit 1
 python3 ./pipeline/stanza/base_pipeline.py to-conll -l en $EN_FILES || exit 1
 echo "... convert files to .vert and compress"
-CONLLU_FILES=$(find -name reliefweb_*.txt.conllu)
+CONLLU_FILES=$(find -name "reliefweb_*.txt.conllu")
 python3 ./pipeline/stanza/base_pipeline.py conll-to-vert $CONLLU_FILES || exit 1
 echo "... rw_corpora_update.sh completed"
