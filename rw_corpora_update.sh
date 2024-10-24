df11260762eb51de2b1cfa4df98c444f25742d0c
engisalor <50170623+engisalor@users.noreply.github.com>
2024-10-24 16:24:11 +0200

    fix: add secondary pipeline to corpora update

--- rw_corpora_update.sh
+++ rw_corpora_update.sh
@@ -18,5 +18,18 @@ python3 ./pipeline/stanza/base_pipeline.py to-conll -l fr $FR_FILES || exit 1
 python3 ./pipeline/stanza/base_pipeline.py to-conll -l en $EN_FILES || exit 1
 echo "... convert files to .vert and compress"
 CONLLU_FILES=$(find -name "reliefweb_*.txt.conllu")
-python3 ./pipeline/stanza/base_pipeline.py conll-to-vert $CONLLU_FILES || exit 1
+python3 ./pipeline/stanza/base_pipeline.py conll-to-vert --no-compress $CONLLU_FILES || exit 1
+EN_VERT_FILES=$(find -name "reliefweb_en*.vert")
+FR_VERT_FILES=$(find -name "reliefweb_fr*.vert")
+ES_VERT_FILES=$(find -name "reliefweb_es*.vert")
+echo "... base_pipeline completed"
+echo "... start secondary pipeline"
+echo "... run sentence-level language identification for ES"
+python3 ./pipeline/stanza/secondary_pipeline.py lingid $ES_VERT_FILES || exit 1
+echo "... update structures for ES, inc. langid"
+python3 ./pipeline/stanza/secondary_pipeline.py main $ES_VERT_FILES --clear --langid || exit 1
+echo "... update structures for EN and FR"
+python3 ./pipeline/stanza/secondary_pipeline.py main $EN_VERT_FILES --clear || exit 1
+python3 ./pipeline/stanza/secondary_pipeline.py main $FR_VERT_FILES --clear || exit 1
+echo "... secondary pipeline completed"
 echo "... rw_corpora_update.sh completed"
